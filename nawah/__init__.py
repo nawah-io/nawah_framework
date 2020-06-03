6239e26661624ae8313a67daf19b056041e55612
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-06-03 11:00:45 +0000

    Updated version

--- nawah/__init__.py
+++ nawah/__init__.py
@@ -1,3 +1,3 @@
 # Ancora Imparo.
 
-__version__ = '0.1.0'
+__version__ = '1.0.0'
--- nawah/packages/core/__init__.py
+++ nawah/packages/core/__init__.py
@@ -3,6 +3,6 @@
 from nawah.classes import PACKAGE_CONFIG
 
 config = PACKAGE_CONFIG(
-	api_level='0.1',
-	version='0.1.0'
+	api_level='1.0',
+	version='1.0.0'
 )
