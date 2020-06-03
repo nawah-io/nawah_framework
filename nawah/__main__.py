121751637ef4774e92ba4feae5b538bebf1368df
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-06-03 10:55:57 +0000

    Updated framework

--- nawah/__main__.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-	from nawah.cli import nawah_cli
-	nawah_cli()
--- nawah/packages/core/__init__.py
+++ nawah/packages/core/__init__.py
@@ -3,6 +3,6 @@
 from nawah.classes import PACKAGE_CONFIG
 
 config = PACKAGE_CONFIG(
-	api_level='6.1',
-	version='6.1.0'
+	api_level='0.1',
+	version='0.1.0'
 )
