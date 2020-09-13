9adaa978566fd39d548221bf334ab2ec79651750
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-09-13 23:08:23 +0400

    Version 1.0.2
    - Introduced Registry to facilitate modules access separated from Config.
    - Dropped default value of attrs for EXTN controller.
    - Refactored framework for changes.
    - Introduced version, api_level setuptools utility install commands.
    - Introduced deploy_version to automate updating wheels.
    - Bumped version to 1.0.2

--- nawah/__init__.py
+++ nawah/__init__.py
@@ -1,3 +1,3 @@
 # Ancora Imparo.
 
-__version__ = '1.0.0'
+__version__ = '1.0.2'
---
+++ nawah/registry.py
@@ -0,0 +1,77 @@
+from nawah.config import Config
+from nawah.classes import NAWAH_MODULE
+
+from typing import Dict, List, Any
+
+import logging
+
+logger = logging.getLogger('nawah')
+
+class InvalidModuleException(Exception):
+	def __init__(self, *, module):
+		self.module = module
+		logger.debug(f'InvalidModuleException: {str(self)}')
+
+	def __str__(self):
+		return f'Invalid module \'{self.module}\''
+
+class InvalidLocaleException(Exception):
+	def __init__(self, *, locale):
+		self.locale = locale
+		logger.debug(f'InvalidLocaleException: {str(self)}')
+
+	def __str__(self):
+		return f'Invalid locale \'{self.locale}\''
+
+class InvalidLocaleTermException(Exception):
+	def __init__(self, *, locale, term):
+		self.locale = locale
+		self.term = term
+		logger.debug(f'InvalidLocaleTermException: {str(self)}')
+
+	def __str__(self):
+		return f'Invalid term \'{self.term}\' of locale \'{self.locale}\''
+
+class InvalidVarException(Exception):
+	def __init__(self, *, var):
+		self.var = var
+		logger.debug(f'InvalidVarException: {str(self)}')
+
+	def __str__(self):
+		return f'Invalid var \'{self.var}\''
+
+class Registry:
+	docs: List[Dict[str, Any]] = Config.docs
+	jobs: List[Dict[str, Any]] = Config.jobs
+
+	@staticmethod
+	def module(module: str) -> NAWAH_MODULE:
+		try:
+			return Config.modules[module]
+		except KeyError:
+			raise InvalidModuleException(module=module)
+	
+	@staticmethod
+	def l10n(*, locale: str, term: str):
+		if locale not in Config.l10n.keys():
+			raise InvalidLocaleException(locale=locale)
+
+		try:
+			if '.' in term:
+				from nawah.utils import extract_attr
+				return extract_attr(scope=Config.l10n[locale], attr_path=term)
+			else:
+				return Config.l10n[locale][term]
+		except (KeyError, TypeError):
+			raise InvalidLocaleTermException(locale=locale, term=term)
+
+	@staticmethod
+	def var(var: str):
+		try:
+			if '.' in var:
+				from nawah.utils import extract_attr
+				return extract_attr(scope=Config.vars, attr_path=var)
+			else:
+				return Config.vars[var]
+		except (KeyError, TypeError):
+			raise InvalidVarException(var=var)
