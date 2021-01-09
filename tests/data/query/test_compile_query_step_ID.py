294becce6702580b629cf852162d768162ed0210
Mahmoud Abduljawad <abduljawad.mahmoud@gmail.com>
2021-01-09 09:59:51 +0000

    Updated tests

--- tests/data/query/test_compile_query_step_ID.py
+++ tests/data/query/test_compile_query_step_ID.py
@@ -27,7 +27,7 @@ def test_compile_query_step_one_step_match_attr_ID_val_invalid_id(caplog):
 	)
 	for record in caplog.records:
 		assert record.levelname == 'WARNING'
-		assert record.message == 'Failed to convert attr to id type: match_term'
+		assert record.message == 'Failed to convert attr to ObjectId: match_term'
 	assert aggregate_prefix == [
 		{'$match': {'__deleted': {'$exists': False}}},
 		{'$match': {'__create_draft': {'$exists': False}}},
@@ -118,19 +118,21 @@ def test_compile_query_step_one_step_match_attr_ID_val_list_invalid_id(caplog):
 		step=step,
 		watch_mode=watch_mode,
 	)
+	# [DOC] Allow caplog.records to exceed 1 in case of other emitter of wanrnings
+	assert len(caplog.records) >= 1
 	for record in caplog.records:
 		assert record.levelname == 'WARNING'
-		assert record.message == 'Failed to convert attr to id type: match_term'
+		assert record.message == 'Failed to convert child_attr to ObjectId: match_term'
 	assert aggregate_prefix == [
 		{'$match': {'__deleted': {'$exists': False}}},
 		{'$match': {'__create_draft': {'$exists': False}}},
 		{'$match': {'__update_draft': {'$exists': False}}},
 	]
 	assert aggregate_suffix == [{'$group': {'_id': '$_id'}}]
-	assert aggregate_match == [{'attr': ['match_term']}]
+	assert aggregate_match == [{'attr': {'$in': ['match_term']}}]
 
 
-def test_compile_query_step_one_step_match_attr_ID_val_list_str():
+def test_compile_query_step_one_step_match_attr_ID_val_list_str(caplog):
 	aggregate_prefix = [
 		{'$match': {'__deleted': {'$exists': False}}},
 		{'$match': {'__create_draft': {'$exists': False}}},
@@ -151,16 +153,21 @@ def test_compile_query_step_one_step_match_attr_ID_val_list_str():
 		step=step,
 		watch_mode=watch_mode,
 	)
+	for record in caplog.records:
+		assert (
+			record.message
+			!= 'Failed to convert child_attr to ObjectId: 000000000000000000000000'
+		)
 	assert aggregate_prefix == [
 		{'$match': {'__deleted': {'$exists': False}}},
 		{'$match': {'__create_draft': {'$exists': False}}},
 		{'$match': {'__update_draft': {'$exists': False}}},
 	]
 	assert aggregate_suffix == [{'$group': {'_id': '$_id'}}]
-	assert aggregate_match == [{'attr': [ObjectId('000000000000000000000000')]}]
+	assert aggregate_match == [{'attr': {'$in': [ObjectId('000000000000000000000000')]}}]
 
 
-def test_compile_query_step_one_step_match_attr_ID_val_list_id():
+def test_compile_query_step_one_step_match_attr_ID_val_list_id(caplog):
 	aggregate_prefix = [
 		{'$match': {'__deleted': {'$exists': False}}},
 		{'$match': {'__create_draft': {'$exists': False}}},
@@ -181,10 +188,15 @@ def test_compile_query_step_one_step_match_attr_ID_val_list_id():
 		step=step,
 		watch_mode=watch_mode,
 	)
+	for record in caplog.records:
+		assert (
+			record.message
+			!= 'Failed to convert child_attr to ObjectId: 000000000000000000000000'
+		)
 	assert aggregate_prefix == [
 		{'$match': {'__deleted': {'$exists': False}}},
 		{'$match': {'__create_draft': {'$exists': False}}},
 		{'$match': {'__update_draft': {'$exists': False}}},
 	]
 	assert aggregate_suffix == [{'$group': {'_id': '$_id'}}]
-	assert aggregate_match == [{'attr': [ObjectId('000000000000000000000000')]}]
+	assert aggregate_match == [{'attr': {'$in': [ObjectId('000000000000000000000000')]}}]
