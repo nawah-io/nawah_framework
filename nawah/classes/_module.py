7c3d87b26f8f552651bb07de12f2a4585d2f52ce
Mahmoud Abduljawad <mahmoud@masaar.com>
2021-06-10 11:26:23 +0000

    Update PERM controller, implement validation methods

--- nawah/classes/_module.py
+++ nawah/classes/_module.py
@@ -15,17 +15,20 @@ from typing import (
 	TypedDict,
 )
 
-import datetime
+import datetime, logging
 
 from ._exceptions import MethodException
 
 if TYPE_CHECKING:
+	from nawah.base_module import BaseModule
 	from nawah.base_method import BaseMethod
 	from ._attr import ATTR
 	from ._dictobj import DictObj
 	from ._types import NAWAH_EVENTS, NAWAH_ENV, NAWAH_DOC, NAWAH_QUERY
 	from ._query import Query
 
+
+logger = logging.getLogger('nawah')
 
 PRE_HANDLER_RETURN = Tuple[
 	'NAWAH_EVENTS', 'NAWAH_ENV', 'Query', 'NAWAH_DOC', Dict[str, Any]
@@ -46,6 +49,9 @@ class PERM:
 	query_mod: PERM_QUERY_MOD
 	doc_mod: PERM_DOC_MOD
 
+	_method: 'METHOD'
+	_set_index: int
+
 	def __repr__(self):
 		return f'<PERM:{self.privilege},{self.query_mod},{self.doc_mod}>'
 
@@ -63,6 +69,44 @@ class PERM:
 		self.privilege = privilege
 		self.query_mod = query_mod
 		self.doc_mod = doc_mod
+
+	def _validate_query_mod(self):
+		if self.query_mod:
+			logger.debug(
+				f'Attemting to validate \'{self._method._module.module_name}.{self._method._method_name}[{self._set_index}].query_mod\'.'
+			)
+			self.__validate_obj(self.query_mod)
+
+	def _validate_doc_mod(self):
+		if self.doc_mod:
+			logger.debug(
+				f'Attemting to validate \'{self._method._module.module_name}.{self._method._method_name}[{self._set_index}].doc_mod\'.'
+			)
+			self.__validate_obj(self.doc_mod)
+
+	def __validate_obj(self, obj):
+		from ._attr import ATTR
+
+		obj_iter: Iterable
+		if type(obj) == list:
+			obj_iter = range(len(obj))
+		elif type(obj) == dict:
+			obj_iter = list(obj.keys())
+		else:
+			Exception(f'Can\'t validate permission obj {obj} of type {type(obj)}.')
+
+		logger.debug(f'Found \'{obj}\' to validate. Iterating.')
+
+		for j in obj_iter:
+			logger.debug(f'Attempting to validate \'{j}\'')
+			if type(obj[j]) in [list, dict]:
+				logger.debug(f'Item is an iterable object. Iterating.')
+				self.__validate_obj(obj[j])
+			elif type(obj[j]) == ATTR:
+				if obj[j]._type != 'TYPE':
+					Exception(f'Attr Type \'{j}\' of TYPE \'{obj[j]._type}\' is not allowed.')
+
+				ATTR.validate_type(attr_type=obj[j])
 
 
 class EXTN:
@@ -110,7 +154,10 @@ class METHOD:
 	get_method: bool
 	post_method: bool
 	watch_method: bool
+
 	_callable: 'BaseMethod'
+	_module: 'BaseModule'
+	_method_name: str
 
 	def __init__(
 		self,
