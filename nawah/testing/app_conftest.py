0cd598b22abe9fa23ba71f222f2e5ad3035442b2
Mahmoud Abduljawad <mahmoud@masaar.com>
2021-06-14 08:10:06 +0000

    Update app_conftest
    - setup_test fixture can now be re-called for every test to recreate data connnection

--- nawah/testing/app_conftest.py
+++ nawah/testing/app_conftest.py
@@ -1,4 +1,5 @@
 from nawah.config import Config
+from nawah.data import create_conn
 from nawah.cli import launch
 from nawah.utils import import_modules, generate_attr
 
@@ -6,10 +7,18 @@ import pytest, argparse
 
 test_env = None
 
+
 @pytest.fixture
 def setup_test():
 	async def _():
 		global test_env
+
+		if test_env:
+			# [DOC] Re-creating data connection is needed as pytest asyncio event loop is scoped per test, making the connection created in a previous test using closed event loop
+			Config._sys_conn = create_conn()
+			Config._sys_env['conn'] = Config._sys_conn
+			test_env['conn'] = Config._sys_conn
+			return
 
 		Config.test = True
 		launch(args=argparse.Namespace(env=None, debug=True), custom_launch='test')
@@ -28,5 +37,5 @@ def env():
 		global test_env
 
 		return test_env
-	
+
 	return _
